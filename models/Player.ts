647b82e7798e94bc61d0bee80802961e1c3ddef0
Trent Jones <trentwilliamjones@gmail.com>
2023-11-17 00:05:16 +0800

    feat: simplify scoring

--- models/Player.ts
+++ models/Player.ts
@@ -1,7 +1,11 @@
+import { DateTime } from 'luxon';
+
 export interface Player {
   name: string;
   goalkeeper: boolean;
   captain: boolean;
   number: number;
-  goals: number;
+  goals: Array<DateTime>; // Number of goals = number of entries.
+  assists: Array<DateTime>; // Number of assists = number of entries.
+  penaltyMinutes: Array<{ time: DateTime; minutes: number }>; // Penalties can have different amount, so need a complex object.
 }
